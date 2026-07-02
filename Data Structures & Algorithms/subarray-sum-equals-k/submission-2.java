200ce35350cc93b96589171d9995e7f5e2263c88
Mohit Gujarati <95397404+MohitGujarati@users.noreply.github.com>
2026-07-02 08:30:44 -0400

    Add: subarray-sum-equals-k - submission-2

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-2.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        int prefixSum = 0;
+        int totalSubarrays = 0;
+        map.put(0, 1);
+        for (int num : nums) {
+            prefixSum += num;
+
+            if (map.containsKey(prefixSum - k)) {
+                totalSubarrays += map.get(prefixSum - k);
+            }
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return totalSubarrays;
+    }
+}
+
+/*
+
+Naive
+
+2 loop i and j
+take an variable as current sum and maxSubarray
+
+if(currentsum == k){
+maxSubarray++
+}
+
+at the end return maxSubarray
+
+//Code
+
+    public int subarraySum(int[] nums, int k) {
+
+        int maxSubarray = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+             int currentSum = 0;
+            for (int j = i; j < nums.length; j++) {
+                currentSum += nums[j];
+
+                if (currentSum == k) {
+                    maxSubarray++;
+                }
+            }
+        }
+
+        return maxSubarray;
+    }
+
+
+*/
